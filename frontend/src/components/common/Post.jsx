765301894c1d9eafef9eca4696f9444a455c35c2
Rutwik2609 <98237672+Rutwik2609@users.noreply.github.com>
2024-11-28 12:43:03 +0530

    Update Post.jsx

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -9,12 +9,16 @@ import { useQueryClient, useMutation, useQuery } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import LoadingSpinner from "../../components/common/LoadingSpinner";
 
+import { formatPostDate } from "../../utils/date";
+
 const Post = ({ post }) => {
 	const [comment, setComment] = useState("");
 	const queryClient = useQueryClient();
 	const formattedDate = "1h";
 	const postOwner = post.user;
 	const { data: authUser } = useQuery({ queryKey: ["authUser"] });
+
+	const formattedDate = formatPostDate(post.createdAt);
 
 	const { mutate: likePost, isPending: isLiking } = useMutation({
 		mutationFn: async () => {
